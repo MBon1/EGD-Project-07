737ba81954c7395087754251302d6ff8298e688c
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2023-04-05 08:43:35 -0400

    distanced lighting

--- EGD-07-3D/Assets/Scripts/LightController.cs
+++ EGD-07-3D/Assets/Scripts/LightController.cs
@@ -13,6 +13,9 @@ public class LightController : MonoBehaviour
     [Header("Audio Loudness Parameters")]
     [SerializeField] float loudnessSensibility = 1;
     [SerializeField] float threshold = 0f;
+    [SerializeField] float changeTime = 0;
+    [SerializeField] GameObject player;
+    [SerializeField] float distanceMultiplier = 1;
 
     private void Awake()
     {
@@ -30,11 +33,41 @@ public class LightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float distanceFromPlayer = 1;
+
+        if (player != null)
+        {
+            distanceFromPlayer = Mathf.Abs(maxRange * distanceMultiplier - DistanceFromPlayer());
+            Debug.Log(distanceFromPlayer);
+            distanceFromPlayer = Mathf.Clamp(distanceFromPlayer, 0, 1);
+        }
+        
+
+
         float loudness = detection.GetLoudnessFromMic() * loudnessSensibility;
 
         if (loudness < threshold)
             loudness = 0;
 
-        light.range = Mathf.Lerp(0, maxRange, loudness);
+        if (changeTime <= 0)
+        {
+            light.range = Mathf.Lerp(0, maxRange, loudness);
+        }
+        else
+        {
+            light.range = Mathf.Lerp(light.range, Mathf.Lerp(0, maxRange, loudness), changeTime * Time.deltaTime);
+        }
+
+
+        light.range *= distanceFromPlayer;
+    }
+
+    float DistanceFromPlayer()
+    {
+        Vector3 pos = this.transform.position;
+        pos.y = 0;
+        Vector3 playerPos = player.transform.position;
+        playerPos.y = 0;
+        return Vector3.Distance(pos, playerPos);
     }
 }
